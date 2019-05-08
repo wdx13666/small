667a893ee93739882c8dec9e66f7dea1dd4047d5
Administrator <872386515@qq.com>
2019-05-08 10:01:12 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import config from './config'
+import MyComponent from './components/MyComponent'
+import './http';
+import 'vuetify/dist/vuetify.min.css'
+import qs from 'qs'
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/material.css'
+
+Vue.use(Vuetify, { theme: config.theme})
+Vue.use(MyComponent)
+Vue.prototype.$qs = qs;
 
 Vue.config.productionTip = false
 
